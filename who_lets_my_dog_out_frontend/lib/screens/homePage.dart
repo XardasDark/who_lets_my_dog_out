d9873ba8d233e26febc48c1d9474a601871d7429
XardasDark <dennispetana@gmail.com>
2022-04-16 04:08:26 +0200

    Added chat and homepage and a background img

---
+++ who_lets_my_dog_out_frontend/lib/screens/homePage.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          repeat: ImageRepeat.repeat,
+          image: AssetImage('assets/background.jpg'),
+        )),
+        child: const Center(child: Text("Startseite")),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey.shade600,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.house),
+            label: "Start",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: "Chats",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            label: "Profil",
+          ),
+        ],
+      ),
+    );
+  }
+}
